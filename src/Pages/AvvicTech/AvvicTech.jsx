7df2f1481f71928f860e8ac41fd27a24929cb495
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-06-05 12:16:36 +0100

    completed tech page with responsiveness

--- src/Pages/AvvicTech/AvvicTech.jsx
+++ src/Pages/AvvicTech/AvvicTech.jsx
@@ -5,6 +5,8 @@ import up from '../../assets/up.png'
 import SectionButton from '../../Components/SectionButton/SectionButton'
 import AvvicTechHero from '../../Components/AvvicTechComponents/AvvicTechHero/AvvicTechHero'
 import Navbar from '../../Components/Navbar/Navbar'
+import AvvicTechOffers from '../../Components/AvvicTechComponents/AvvicTechOffers/AvvicTechOffers'
+import Footer from '../../Components/Footer/Footer'
 
 const AvvicTech = () => {
   return (
@@ -17,6 +19,10 @@ const AvvicTech = () => {
         </div>
 
         <AvvicTechHero />
+
+        <AvvicTechOffers />
+
+        <Footer />
 
         <SectionButton to={'tech-hero'} className={'tech-btn'} image={up}/>
     </div>
